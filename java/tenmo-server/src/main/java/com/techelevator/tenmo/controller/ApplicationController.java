6199b4d23a785ed0087074a16f36769c28f1b071
Brittany Wheeler-Wolowicz <nannyhap@icloud.com>
2021-08-20 12:49:11 -0500

    safety push

--- java/tenmo-server/src/main/java/com/techelevator/tenmo/controller/ApplicationController.java
+++ java/tenmo-server/src/main/java/com/techelevator/tenmo/controller/ApplicationController.java
@@ -2,6 +2,8 @@ package com.techelevator.tenmo.controller;
 
 
 import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.dao.TransferDao;
+import com.techelevator.tenmo.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +11,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@PreAuthorize("isAuthenticated")
+@PreAuthorize("isAuthenticated()")
 public class ApplicationController {
 
 
      @Autowired
-     AccountDao dao;
+     AccountDao accountDao;
 
-   //  @RequestMapping (path= "/balance",method = RequestMethod.GET,httpEntity);
+     @Autowired
+     TransferDao transferDao;
+
+     @Autowired
+     UserDao userDao;
+
+   //  @RequestMapping (path= "/balance", method = RequestMethod.GET,httpEntity);
+
+   //  @RequestMapping (path= "/transfers", method = RequestMethod.GET,httpEntity);
+
+   //  @RequestMapping (path= "/transfers/send", method = RequestMethod.POST,httpEntity);
+
+   //  @RequestMapping (path= "/transfers/request", method = RequestMethod.POST,httpEntity);
+
+   //  @RequestMapping (path= "/transfers/approve", method = RequestMethod.PUT,httpEntity);
 
 
 }
